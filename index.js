e4eb3aea3b0c58fd897b52bfc38835b8a8aa2441
Onegreat <wangreat@yandex.ru>
2023-01-09 20:23:00 +0300

    ДЗ

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Тест');
+const getData = () => {
+    return fetch('db.json')
+        .then(response => response.json())
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+const sendData = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .catch(error => console.log(error));
+}
+
+let user;
+
+getData()
+    .then(data => {
+        user = data;
+        console.log(user);
+        return sendData('https://jsonplaceholder.typicode.com/posts', user);
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
